cd14fb6e92170c60fe6dd629cab0d65cdcaa4c47
princesssuna <1009suna@gmail.com>
2021-01-21 22:33:14 +0900

    북마크 디자인 추가

--- app/src/main/java/com/example/everywhere/SearchActivity.kt
+++ app/src/main/java/com/example/everywhere/SearchActivity.kt
@@ -6,6 +6,7 @@ import android.content.Context
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
@@ -13,6 +14,7 @@ import androidx.fragment.app.Fragment
 import com.example.everywhere.models.Document
 import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_search.*
+import kotlinx.android.synthetic.main.recyclerview_item.*
 
 open class SearchActivity : AppCompatActivity(),
     SearchLocalConstants.View {
@@ -23,6 +25,17 @@ open class SearchActivity : AppCompatActivity(),
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        /*star_bookmark.setOnClickListener(object: View.OnClickListener {
+            var isDefault=true
+            override fun onClick(v: View?) {
+                isDefault=!isDefault
+                if(isDefault) star_bookmark.setImageResource(R.drawable.star_bookmark1)
+                else star_bookmark.setImageResource(R.drawable.star_bookmark2)
+            }
+
+        })*/
+
 
         /*search_view.setOnClickListener { view ->
             val searchStr = search_view.query.toString()
@@ -91,4 +104,9 @@ open class SearchActivity : AppCompatActivity(),
     override fun showError(error: String) {
         Toast.makeText(this, error, Toast.LENGTH_SHORT).show()
     }
+
+    /*fun star_click(view: View?) {
+        val imageView: ImageView = findViewById(R.id.star_bookmark) as ImageView
+        imageView.setImageResource(R.drawable.star_bookmark2)
+    }*/
 }
